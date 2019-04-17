48857cf042e8d4a123e688e064ac2ddcc25bc2d1
SOLYMAR <SOLYMAR@SOLYMAR-PC>
2019-04-16 22:58:35 -0500

    1

---
+++ Tesis004/Tesis004/Controllers/EnfermeriaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tesis004.Controllers
+{
+    public class EnfermeriaController : Controller
+    {
+		// GET: Enfermeria
+		public ActionResult IngresarSignosVitales()
+		{
+			return View();
+		}
+
+		public ActionResult IngresarVacunas()
+		{
+			return View();
+		}
+	}
+}
---
+++ Tesis004/Tesis004/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Tesis004.MvcApplication" Language="C#" %>
