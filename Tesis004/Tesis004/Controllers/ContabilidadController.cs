35fe245b2c8f0c7193d6b482b357a809c1b36a31
SOLYMAR <SOLYMAR@SOLYMAR-PC>
2019-05-15 22:40:07 -0500

    1

---
+++ Tesis004/Tesis004/Controllers/ContabilidadController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tesis004.Controllers
+{
+    public class ContabilidadController : Controller
+    {
+		// GET: Contabilidad
+		public ActionResult GestionContabilidad()
+		{
+			return View();
+		}
+
+	}
+}
