a7b5284e261f9e52fd25ff45cdc34373b627ccf1
SOLYMAR <SOLYMAR@SOLYMAR-PC>
2019-05-15 23:43:12 -0500

    2

---
+++ Tesis004/Tesis004/Controllers/InventarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tesis004.Controllers
+{
+    public class InventarioController : Controller
+    {
+        // GET: Inventario
+        public ActionResult GestionInventario()
+        {
+            return View();
+        }
+    }
+}
---
+++ Tesis004/Tesis004/Controllers/PersonalController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tesis004.Controllers
+{
+    public class PersonalController : Controller
+    {
+        // GET: Personal
+        public ActionResult GestionPersonal()
+        {
+            return View();
+        }
+
+		public ActionResult ModificarPersonal()
+		{
+			return View();
+		}
+
+		public ActionResult GestionServiciosMedicos()
+		{
+			return View();
+		}
+
+		public ActionResult ModificarServicioMedicos()
+		{
+			return View();
+		}
+	}
+}
