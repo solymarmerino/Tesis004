db21cf0d1bad30a7f67b7c85c629bad4c9c295c9
SOLYMAR <SOLYMAR@SOLYMAR-PC>
2019-09-30 23:18:04 -0500

    1

---
+++ Tesis004/Tesis004/Models/ConsultaMedica.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tesis004.Models
+{
+    public class ConsultaMedicaModel
+    {
+        public int ConsultaMedicaID { get; set; }
+		public string MotivoConsulta { get; set; }
+		public string EnfermedadProblemaActual { get; set; }
+		public string Analisis { get; set; }
+		public string PlanTratamiento { get; set; }
+		public DateTime FechaConsulta { get; set; }
+		public int TipoConsulta { get; set; }
+		public int HistoriaClinicaID { get; set; }
+		public int DiagnosticoID { get; set; }
+		public int ObjetivoID { get; set; }
+		public int RecetaID { get; set; }
+		public int SignoVitalID { get; set; }
+		public int SubjetivoID { get; set; }
+	}
+}
---
+++ Tesis004/Tesis004/Models/HistoriaClinicaModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tesis004.Models
+{
+    public class HistorisClinicaModel
+    {
+		public int HistoriaClinicaID { get; set; }
+		public string AntecedentesPersonales { get; set; }
+		public string AntecedenesFamiliares { get; set; }
+		public string AntecedetesSociales { get; set; }
+		public string Habitos { get; set; }
+		public string Alergias { get; set; }
+		public int PacienteID { get; set; }
+	}
+}
