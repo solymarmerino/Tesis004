ef6e304e29172898ce99c72d602fa7e423e2e57b
Daniel Guaman Pullas <DGuamanP@atimasa.com.ec>
2019-10-16 23:23:50 -0500

    HC 02

--- Tesis004/Tesis004/Scripts/historia-clinica.js
+++ Tesis004/Tesis004/Scripts/historia-clinica.js
@@ -28,6 +28,33 @@ function consultarSignosVitales() {
     $.ajax(ConsultarSignosVitales);
 }
 
+function guardarDatosConsulta() {
+    var TipoConsultaHC = "";
+    if ($("#Primera").is(':checked')) {
+        TipoConsultaHC: "Primera";
+    }
+    if ($("#Subsecuente").is(':checked')) {
+        TipoConsultaHC: "Subsecuente";
+    }
+    var DatosConsulta = {};
+    DatosConsulta.url = "/HistoriaClinica/GuardarDatosConsulta";
+    DatosConsulta.type = "POST";
+    DatosConsulta.data = JSON.stringify({
+        numeroHistoriaClinica: $("#IptNumeroHistoriaClinica").val(),        
+        tipoConsulta: TipoConsultaHC,
+        motivoConsulta: $("#motivoconsulta").val()
+    });
+    DatosConsulta.datatype = "json";
+    DatosConsulta.contentType = "application/json";
+    DatosConsulta.success = function (resultado) {
+        toastr.error("Error al consultar los signos vitales");
+    };
+    DatosConsulta.error = function () {
+        toastr.error("Error al consultar los signos vitales");
+    };
+    $.ajax(DatosConsulta);
+}
+
 $(document).ready(function () {
     consultarSignosVitales();
 });
