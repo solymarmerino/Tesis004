205bf97320c555ea928e060982570bb2e7afa5eb
solymarmerino <solymar.merino@epn.edu.ec>
2019-10-20 10:45:24 -0500

    modelos

---
+++ Tesis004/Tesis004/Models/CertificadoMedicoModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tesis004.Models
+{
+    public class CertificadoMedicoModel
+	{
+        public int CertificadoMedicoID { get; set; }
+		public DateTime FechaInicio { get; set; }
+		public DateTime FechaFin { get; set; }
+		public DateTime FechaCertificado { get; set; }
+		public string Observaciones { get; set; }
+		public int ConsultaMedicaID { get; set; }
+	}
+}
